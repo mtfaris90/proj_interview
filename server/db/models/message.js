42588b4912e59a560a0bd9344bd931b3b88948ba
Matthew Faris <mtfaris90@gmail.com>
2021-07-23 15:43:18 -0400

    add hasBeenRead column to message table schema

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -10,6 +10,11 @@ const Message = db.define("message", {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
+  hasBeenRead: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  }
 });
 
 module.exports = Message;
