6d73ea038d6a909153fe2acf29254fbaf0aa213c
Matthew Faris <mtfaris90@gmail.com>
2021-07-23 12:52:19 -0400

    handle invalid conversationId

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
 
-// expects {recipientId, text, conversationId } in body (conversationId will be undefined if no conversation exists yet)
+// expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
   try {
     if (!req.user) {
@@ -11,18 +11,21 @@ router.post("/", async (req, res, next) => {
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
 
-    // if we already know conversation id, we can save time and just add it to message and return
-    if (conversationId !== undefined) {
-      const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
-    }
-    // if we don't have conversation id, find a conversation to make sure it doesn't already exist
+    // find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
       senderId,
       recipientId
     );
 
-    if (conversation === null) {
+    // if we already know a valid conversation id and convo exists between the two users, we can just add it to message and return
+    if (conversation) {
+      if (conversationId === conversation.id) {
+        const message = await Message.create({ senderId, text, conversationId });
+        return res.json({ message, sender });
+      } else {  // send error response and return if the two ids don't match
+        return res.sendStatus(401);
+      }
+    } else {
       // create conversation
       conversation = await Conversation.create({
         user1Id: senderId,
