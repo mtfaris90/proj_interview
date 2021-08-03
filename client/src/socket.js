781131ae2fae7b7a6e298b457ffb29902f73c82a
Matthew Faris <mtfaris90@gmail.com>
2021-08-03 12:58:14 -0400

    change overlapping event names and add socket disconnect logging

--- client/src/socket.js
+++ client/src/socket.js
@@ -21,12 +21,16 @@ socket.on("remove-offline-user", (id) => {
   store.dispatch(removeOfflineUser(id));
 });
 
-socket.on("new-message", (data) => {
+socket.on("message-from-server", (data) => {
   store.dispatch(setNewMessage(data.message, data.sender, data.recipientId));
 });
 
-socket.on("convo-read", (data) => {
+socket.on("convo-read-from-server", (data) => {
   store.dispatch(readConversation(data.conversationId, data.otherUserId));
 });
+
+socket.on("disconnect", () => {
+  console.log("disconnected from server");
+})
 
 export default socket;
