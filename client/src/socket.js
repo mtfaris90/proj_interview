306305a393d85d46bfd7dc2745e243eb5bd35c98
Matthew Faris <mtfaris90@gmail.com>
2021-08-08 21:06:46 -0400

    add checks to prevent unnecessary state updates

--- client/src/socket.js
+++ client/src/socket.js
@@ -9,25 +9,54 @@ import {
 
 const socket = io(window.location.origin);
 
+let user = undefined;
+let conversations = [];
+
 socket.on("connect", () => {
-  console.log("connected to server w/ id ", socket.id);
+  const state = store.getState();
+  user = state.user;
+  conversations = state.conversations;
 });
 
 socket.on("add-online-user", (id) => {
+  // ignore event if other user is already online OR there is no convo with other user
+  if (
+    !conversations.some(
+      (convo) => convo.otherUser.id === id && convo.otherUser.online === false
+    )
+  ) {
+    return;
+  }
   store.dispatch(addOnlineUser(id));
 });
 
 socket.on("remove-offline-user", (id) => {
+  // ignore event if other user is already offline OR there is no convo with other user
+  if (
+    !conversations.some(
+      (convo) => convo.otherUser.id === id && convo.otherUser.online === true
+    )
+  ) {
+    return;
+  }
   store.dispatch(removeOfflineUser(id));
 });
 
 socket.on("message-from-server", (data) => {
+  // ignore event if message isn't for this user
+  if (user.id !== data.recipientId) {
+    return;
+  }
   store.dispatch(
     setNewMessage(data.message, data.sender, data.recipientId, data.senderId)
   );
 });
 
 socket.on("convo-read-from-server", (data) => {
+  // ignore event if if this user doesn't have the conversation
+  if (!conversations.some((convo) => convo.id === data.conversationId)) {
+    return;
+  }
   store.dispatch(
     readConversation(data.conversationId, data.otherUserId, data.convoLength)
   );
@@ -35,6 +64,8 @@ socket.on("convo-read-from-server", (data) => {
 
 socket.on("disconnect", () => {
   console.log("disconnected from server");
+  user = undefined;
+  conversations = [];
 });
 
 export default socket;
